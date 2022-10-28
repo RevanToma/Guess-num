1e5eefc950db1ac5801a663462437f92a6e09aec
RevanToma <tamrevan@gmail.com>
2022-10-28 14:47:19 +0200

    modified the game and added extra futures

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-const container = document.querySelector('.container');
 const h1 = document.querySelector('h1');
 const guessBtn = document.querySelector('#guess');
 const previousGuesses = document.querySelector('#previousGuesses');
 const again = document.querySelector('#again');
+let input = document.querySelector('#input').value;
 
 
 // skriva ut siffran som skrivs in i inputfältet när man trycker på knappen 'Guess';
 
-
-
+// guessCounter = 0;
 let radnomNum = Math.floor(Math.random() * 100);
-
+guessCounter = 0;
 
 guessBtn.addEventListener('click', () => {
-    let guessNum = document.querySelector('#guessNum').value;
-    if (guessNum < 1 || guessNum > 100) {
+    const high = '(num to high)';
+    const low = '(num to low)';
+    let input = document.querySelector('#input').value;
+    if (input < 1 || input > 100) {
         h1.innerHTML = 'Please enter a number between 1- 100';
-    }
-    else if (guessNum == radnomNum) {
-        h1.innerHTML = `You guessed right, your number was ${radnomNum}`
-
 
     }
-    else if (guessNum < radnomNum) {
+    else if (input == radnomNum) {
+        h1.innerHTML = `You guessed right, your number was: ${radnomNum}`
+        guessBtn.setAttribute('disabled', '');
+
+    }
+    else if (input < radnomNum) {
         h1.innerHTML = 'You guessed to low!'
-        previousGuesses.textContent = guessNum;
+        createLi(`${input} ${low}`);
+        guessCounter++;
+        guessIncrement(guessCounter);
 
-    } else if (guessNum > radnomNum) {
-
+    } else if (input > radnomNum) {
         h1.innerHTML = 'You guessed to high!';
-        previousGuesses.textContent = guessNum;
-    } else if (guessNum > 100) {
+        createLi(`${input} ${high}`);
+        guessCounter++;
+        guessIncrement(guessCounter);
+
+    } else if (input > 100) {
         h1.innerHTML = 'Number must be between 1-100';
+
     } else {
 
         alert('Please enter a number and not a text');
@@ -42,3 +49,18 @@ again.addEventListener('click', () => {
     location.reload();
 })
 
+
+// create a list to show previous guesses.
+function createLi(input) {
+    const li = document.createElement('li');
+    li.textContent = input;
+    previousGuesses.appendChild(li);
+}
+// increase the guess counter and if reached max guesses allowed = game over.
+function guessIncrement(guessCounter) {
+    if (guessCounter === 5) {
+        guessBtn.setAttribute('disabled', '');
+        h1.textContent = 'You lost! play again if you dare!';
+        h1.style.color = 'red';
+    }
+}
